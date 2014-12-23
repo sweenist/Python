b394ed63078a3a69ef87a52346a53042aaae6b39
Ryan Sweeney <sween119@hotmail.com>
2014-12-22 22:08:12 -0500

    Completed Basic 3D Cursor Toggle
    Stripped down functional and clean version of the Cursor Toggle Addon

--- Blender Addons/3D_cursor_toggle_relocate/Toggle_3dCursor_OnClick.py
+++ Blender Addons/3D_cursor_toggle_relocate/Toggle_3dCursor_OnClick.py
@@ -22,19 +22,25 @@ bl_info = {
     "author": "Ryan Sweeney",
     "version": (1, 0),
     "blender": (2, 72, 0),
-    "location": "View3D > Properties Panel > 3D Cursor",
+    "location": "View3D > Properties Panel > 3D Cursor Toggle",
     "description": "Adds a toggle to the 3D Cursor in the properties tab.",
     "warning": "",
     "wiki_url": "http://sweenist.wordpress.com",
     "category": "3D View"}
 
 import bpy
+from bpy.types import Panel
+from bpy.props import BoolProperty
 
-class VIEW3D_PT_view3d_cursor_toggle(VIEW3D_PT_view3d_cursor):
+#class VIEW3D_PT_view3d_cursor_toggle(VIEW3D_PT_view3d_cursor):
+class VIEW3D_PT_view3d_cursor_toggle(Panel):
     """Turns Mouse Action on/off for placing 3D Cursor"""
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_label = "3D Cursor"
+    bl_label = "3D Cursor Toggle"
+    bl_idname = "view3d.cursor3dtoggle"
+    
+    #cursor_toggle_key = BoolProperty(default=True, name="3d_cursor_click_toggle")
     
     @classmethod
     def poll(cls, context):
@@ -44,10 +50,17 @@ class VIEW3D_PT_view3d_cursor_toggle(VIEW3D_PT_view3d_cursor):
     def draw(self, context):
         wm = context.window_manager
         keymaps = wm.keyconfigs['Blender User'].keymaps['3D View'].keymap_items
-        cursor_key = keymaps['view3d.cursor3d'].active
+        cursor_key = keymaps['view3d.cursor3d']
         layout = self.layout
         
-        row = layout.row()
-        row.prop(keymaps, "view3d.cursor3d", text = "Toggle LMB")
-        
+        col = layout.column()
+        col.prop(cursor_key, "active", text="Toggle LMB", toggle=True)
+
+def register():
+    bpy.utils.register_class(VIEW3D_PT_view3d_cursor_toggle)
     
+def unregister():
+    bpy.utils.unregister_class(VIEW3D_PT_view3d_cursor_toggle)
+    
+if __name__ == "__main__":
+    register()
